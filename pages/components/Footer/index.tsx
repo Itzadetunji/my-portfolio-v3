d431cef7a4c8f400835004059e21f7cbb3df35d0
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-08-20 20:18:07 +0100

    Feat: Completed Navbar and Footer Component🤩

--- pages/components/Footer/index.tsx
+++ pages/components/Footer/index.tsx
@@ -1,8 +1,27 @@
 import { NextPage } from "next";
 import React from "react";
+import styles from "../../../styles/Footer.module.css";
 
 const Footer: NextPage = () => {
-	return <div></div>;
+	return (
+		<div className={styles.footerContainer}>
+			<div className={styles.footerContents}>
+				<p>
+					{" "}
+					Copyright <span className={styles.footerSpan}>
+						©
+					</span> 2022{" "}
+					<span
+						className={styles.footerSpan}
+						style={{ backgroundColor: "transparent" }}
+					>
+						|
+					</span>{" "}
+					Adetunji Adeyinka{" "}
+				</p>
+			</div>
+		</div>
+	);
 };
 
 export default Footer;
