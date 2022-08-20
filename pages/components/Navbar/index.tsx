cb49dc1b1ac9ce23e34253476b29cf239d6ff812
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-08-20 14:20:17 +0100

    Chore: Added logo icon

--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -3,9 +3,16 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import NavItem from "./NavItem";
+import styles from "../../../styles/Home.module.css"
 
 const Index: NextPage = () => {
-	return <nav></nav>;
+	return (
+		<nav className={styles.navContainer}>
+			<div className={styles.navContents}>
+				<img src="../icons/" alt="" />
+			</div>
+		</nav>
+	);
 };
 
 export default Index;
