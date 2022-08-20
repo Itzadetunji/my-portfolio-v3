e86e1d21f3c54b689422c26aed9a6c7d849f47c9
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-08-20 14:53:17 +0100

    Feat: Updated navbar Chore: Updated navbar folder with icons

--- pages/components/Navbar/NavItem/index.tsx
+++ pages/components/Navbar/NavItem/index.tsx
@@ -1,9 +1,33 @@
 import { NextPage } from "next";
 import Link from "next/link";
+import { useRouter } from "next/router";
 import React from "react";
+import styles from "../../../../styles/Navbar.module.css";
 
-const index: NextPage = () => {
-	return <p></p>;
+interface NavItemProps {
+	title: string;
+	path: string;
+}
+
+const Index: React.FC<NavItemProps> = ({ title, path }) => {
+	const router = useRouter().pathname;
+	return (
+		<div>
+			<Link href={path}>
+				<p
+					className={`${
+						router.includes("about") &&
+						router.includes("contact") &&
+						router.includes("portfolio")
+							? styles.navItemActive
+							: ''
+					}`}
+				>
+					{title}
+				</p>
+			</Link>
+		</div>
+	);
 };
 
-export default index;
+export default Index;
--- pages/components/Navbar/index.tsx
+++ pages/components/Navbar/index.tsx
@@ -3,13 +3,32 @@ import { useRouter } from "next/router";
 import Link from "next/link";
 import { useEffect, useState } from "react";
 import NavItem from "./NavItem";
-import styles from "../../../styles/Home.module.css"
+import styles from "../../../styles/Navbar.module.css";
 
 const Index: NextPage = () => {
 	return (
 		<nav className={styles.navContainer}>
 			<div className={styles.navContents}>
-				<img src="../icons/" alt="" />
+				<img
+					src="../icons/nav-icons/logo.svg"
+					alt=""
+					className={styles.navLogo}
+				/>
+				<img
+					src="../icons/nav-icons/hamburger.svg"
+					alt=""
+					className={styles.navHamburger}
+				/>
+				<div className={styles.navLinks}>
+					<NavItem title={"Home"} path={"/"} />
+					<NavItem title={"About"} path={"/about"} />
+					<NavItem title={"Portfolio"} path={"/portfolio"} />
+					<NavItem title={"Contact"} path={"/contact"} />
+				</div>
+				<div className={styles.navSocialIcons}>
+					<img src="../icons/social-icons/github.svg" alt="" />
+					<img src="../icons/social-icons/linkedin.svg" alt="" />
+				</div>
 			</div>
 		</nav>
 	);
