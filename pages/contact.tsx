abcd3ec7818f5eb0ceb3e3a4a3adf3b7e51ab6f6
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-09-16 09:45:08 +0100

    Feat: Completed UI for contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -48,41 +48,50 @@ const Contact = () => {
 	return (
 		<>
 			<Navbar />
-			<div className="mx-8">
-				<div className="flex flex-col">
-					<div className="flex flex-col space-y-10">
+			<div className="px-8 text-white max-w-[1440px] mx-auto">
+				<p className="sectionTitle">Let&apos;s Chat</p>
+				<div className="mt-14 flex flex-col flex-1 md:flex-row space-y-6 md:space-y-0 md:justify-between md:space-x-10">
+					<div className="flex flex-col space-y-6 flex-grow">
 						<input
 							type="text"
-							className="w-full box-border pl-6 py-4 bg-gray rounded-full outline-none text-white text-xl placeholder:text-white placeholder:text-xl"
+							className="w-full box-border pl-6 py-7 bg-gray rounded-md outline-none text-white placeholder:text-white"
 							placeholder="What is your name?"
 							maxLength={25}
 						/>
 						<input
 							type="text"
-							className="w-full box-border pl-6 py-4 bg-gray rounded-full outline-none text-white text-xl placeholder:text-white placeholder:text-xl"
+							className="w-full box-border pl-6 py-7 bg-gray rounded-md outline-none text-white placeholder:text-white"
 							placeholder="Why are you sending this?"
 							maxLength={50}
 						/>
 						<input
 							type="text"
-							className="w-full box-border pl-6 py-4 bg-gray rounded-full outline-none text-white text-xl placeholder:text-white placeholder:text-xl"
+							className="w-full box-border pl-6 py-7 bg-gray rounded-md outline-none text-white placeholder:text-white"
 							placeholder="What’s your email?"
 							maxLength={50}
 						/>
 					</div>
-				</div>
-				<div>
-					<textarea
-						name=""
-						className="mt-10 w-full bg-gray px-6 py-4 h-64 rounded-3xl resize-none text-white text-xl placeholder:text-white placeholder:text-xl outline-none"
-						placeholder="What do you want to talk about?"
-					></textarea>
+					<div className="flex flex-col space-y-6 flex-grow">
+						<textarea
+							name=""
+							className="w-full bg-gray box-border px-6 py-7 min-h-[184px] rounded-md resize-none text-white placeholder:text-white outline-none"
+							placeholder="What do you want to talk about?"
+						></textarea>
+						<input
+							type="submit"
+							id="submit"
+							className="mx-auto md:mx-0 custom-shadow rounded-md text-center bg-gray w-44 h-20 duration-500 ease-in-out cursor-pointer"
+							value="Send Message"
+						/>
+					</div>
+					<div className="mt-20 hidden md:block">
+						<SocialLinks />
+					</div>
 				</div>
 			</div>
-			<div className="mt-20">
-				<SocialLinks />
+			<div className="md:fixed md:bottom-0 md:right-1/2 md:translate-x-1/2">
+				<Footer />
 			</div>
-			<Footer />
 		</>
 	);
 };
