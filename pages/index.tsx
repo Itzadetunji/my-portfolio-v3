7131e0ef0adb82cb5d3b9b50cc19e9d44b2bf2be
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-11-20 15:25:06 +0100

    Feat: Started work on navabr and added icon for background image and added class

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,18 @@
 import type { NextPage } from "next";
 // import styles from "../styles/Home.module.css";
 import Link from "next/link";
+import { useEffect } from "react";
 import { Button, Footer, Navbar, SideSocialLinks, SocialLinks } from "../components";
 
 const Index: NextPage = () => {
-	return <></>;
+	useEffect(() => {
+		document.body.style.backgroundImage = `url('/images/background.svg')`;
+		document.body.classList.add("indexBodyImage");
+	}, []);
+	return (
+		<>
+			<div className="container w-[100%] h-[100vh]"></div>
+		</>
+	);
 };
 export default Index;
