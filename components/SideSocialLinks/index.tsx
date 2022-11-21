2944e73d752547304a97668b615a776012b3647c
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-11-21 18:26:49 +0100

    Chore: Added files for social icons and updated side social links and index page

--- components/SideSocialLinks/index.tsx
+++ components/SideSocialLinks/index.tsx
@@ -1,7 +1,14 @@
 import { NextPage } from "next";
 import Link from "next/link";
 import React from "react";
+import { Line } from "../";
 
 export const SideSocialLinks: NextPage = () => {
-	return <div></div>;
+	return (
+		<div>
+			<div className="mt-5 h-[5px] w-[100px] rotate-90">
+				<Line />
+			</div>
+		</div>
+	);
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -16,10 +16,10 @@ const Index: NextPage = () => {
 		<>
 			<div className="mx-[40px] md:mx-[85px]">
 				<Navbar />
-				<section className="container flex items-center justify-between">
+				<section className="container max-w-[1270px] mx-auto flex items-center justify-between">
 					<div className="text-white text-[64px] font-bold mt-5 pt-0 relative">
 						<p className="welcomeText text-[230px] text-gray tracking-[-2%] font-bold opacity-50 ml-[-14px] mt-[-77px] absolute">Hello</p>
-						<div className="mt-[197px]">
+						<div className="mt-[197px] max-w-[430px]">
 							<p>
 								I&apos;m <br />
 								Tunji <span className="text-blue">Adeyinka</span>
@@ -29,12 +29,12 @@ const Index: NextPage = () => {
 							</div>
 						</div>
 						<h2 className="text-white font-light text-2xl mt-5">Fullstack Web Developer</h2>
-						<div className="mt-24 max-w-[550px]">
+						<div className="mt-20 max-w-[550px]">
 							<h2 className="font-regular text-[24px] text-[#959DA9]">Lorem ipsum</h2>
 							<h2 className="font-regular text-[24px]">&quot;Code is like humor. When you have to explain it, it’s bad.&quot;</h2>
 						</div>
 					</div>
-					<div className="bg-blue w-[400px] h-[400px] rotate-45">f</div>
+					<div className="bg-blue flex-1 h-[400px] max-w-[400px] "></div>
 					{/* <div className="relative">
 						<div className="">
 							<img
@@ -56,7 +56,7 @@ const Index: NextPage = () => {
 							</svg>
 						</div>
 					</div> */}
-					<div className="h-[500px] bg-blue">f</div>
+					<SideSocialLinks />
 				</section>
 			</div>
 		</>
