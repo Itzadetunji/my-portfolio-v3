4301a0bce8df429ebed8ad4db51e812fd1f85c52
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-20 16:01:22 +0100

    Chore: Chore

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,5 @@
 import { NextPage } from "next";
 import React from "react";
-import Link from "next/link";
 import styles from "../styles/About.module.css";
 import Head from "next/head";
 import { Button, ContactMe, Footer, LargeSocialLinks, Line, Navbar, ServiceCard, SideSocialLinks, SocialLinks, TechStack, TestimonialCard, WorkProcessCard } from "../components";
