b346b047b4fc0aa445a41049a431e6130ef68a0d
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-31 17:14:39 +0100

    Chore: Added ease in and out to section title animation

--- components/SectionTitle/index.tsx
+++ components/SectionTitle/index.tsx
@@ -13,7 +13,7 @@ export const SectionTitle: NextPage<SectionTitleProps> = ({ text }) => {
 			onMouseEnter={() => setIsHover(true)}
 			onMouseLeave={() => setIsHover(false)}
 		>
-			<p className={`z-0 text-center text-[56px] font-bold text-[#363C42] duration-200 ${isHover ? "tracking-widest" : "tracking-normal"}`}>{text}</p>
+			<p className={`z-0 text-center text-[56px] font-bold text-[#363C42] duration-200 ease-in-out ${isHover ? "tracking-widest" : "tracking-normal"}`}>{text}</p>
 			<p className="absolute z-10 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl font-bold">{text}</p>
 		</div>
 	);
