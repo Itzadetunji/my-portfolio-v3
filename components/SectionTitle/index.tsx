e52f2e782ea52bde6c12fe8f0a1835a627bc5818
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-31 18:03:34 +0100

    Feat: Creating change of about me

--- components/SectionTitle/index.tsx
+++ components/SectionTitle/index.tsx
@@ -9,12 +9,12 @@ export const SectionTitle: NextPage<SectionTitleProps> = ({ text }) => {
 	const [isHover, setIsHover] = useState<boolean>();
 	return (
 		<div
-			className="relative text-white wide-text-animation"
+			className="relative text-white wide-text-animation h-max"
 			onMouseEnter={() => setIsHover(true)}
 			onMouseLeave={() => setIsHover(false)}
 		>
-			<p className={`z-0 text-center text-[56px] font-bold text-[#363C42] duration-200 ease-in-out ${isHover ? "tracking-widest" : "tracking-normal"}`}>{text}</p>
-			<p className="absolute z-10 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl font-bold">{text}</p>
+			<p className={`absolute z-0 text-center top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] font-bold text-[#363C42] duration-200 ease-in-out ${isHover ? "text-[56px] tracking-widest" : " text-4xl tracking-normal"}`}>{text}</p>
+			<p className="absolute z-10 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl font-bold text-center">{text}</p>
 		</div>
 	);
 };
--- pages/about.tsx
+++ pages/about.tsx
@@ -9,7 +9,19 @@ const About: NextPage = () => (
 	<>
 		{/* <Line /> */}
 		<div className="mx-[40px] sm:mx-[50px] xl:mx-[85px]">
-			<SectionTitle text={"About Me"} />
+			<section className="mt-12 text-white">
+				<SectionTitle text={"About Me"} />
+				<div className="pt-16 flex space-x-[60px] justify-between">
+					<div className="flex-1 bg-gray" />
+					<div>
+						<h1 className="font-bold my-8">I&apos;m a FULL-STACK DEVELOPER</h1>
+					</div>
+					<div className="flex justify-between items-center min-w-[106px]">
+						d
+						<hr className="bg-white grow h-[1px]" />d
+					</div>
+				</div>
+			</section>
 		</div>
 	</>
 );
