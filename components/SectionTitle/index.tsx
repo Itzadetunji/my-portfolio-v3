7ebf9c1af7b78bdd85e3af2003a257cb76c7aa6a
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2022-12-31 17:12:42 +0100

    Chore: Made text spacing wider

--- components/SectionTitle/index.tsx
+++ components/SectionTitle/index.tsx
@@ -8,8 +8,12 @@ interface SectionTitleProps {
 export const SectionTitle: NextPage<SectionTitleProps> = ({ text }) => {
 	const [isHover, setIsHover] = useState<boolean>();
 	return (
-		<div className="relative text-white wide-text-animation">
-			<p className={`z-0 text-center text-[56px] font-bold text-[#363C42] duration-200 ${isHover ? "tracking-wider" : "tracking-normal"}`}>{text}</p>
+		<div
+			className="relative text-white wide-text-animation"
+			onMouseEnter={() => setIsHover(true)}
+			onMouseLeave={() => setIsHover(false)}
+		>
+			<p className={`z-0 text-center text-[56px] font-bold text-[#363C42] duration-200 ${isHover ? "tracking-widest" : "tracking-normal"}`}>{text}</p>
 			<p className="absolute z-10 top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] text-4xl font-bold">{text}</p>
 		</div>
 	);
