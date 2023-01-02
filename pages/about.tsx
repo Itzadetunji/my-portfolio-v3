d12233a0d5f49db158dcdd3371561f3748daa03f
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-02 04:46:31 +0100

    Feat: Addded resume file and finished up about state

--- pages/about.tsx
+++ pages/about.tsx
@@ -15,74 +15,78 @@ const About: NextPage = () => {
 		<>
 			<section className="flex flex-col mt-12">
 				<SectionTitle text={"About Me"} />
-			</section>
-			<div className="flex flex-col items-center mx-[40px] sm:mx-[50px] xl:mx-[85px]">
-				<div className="pt-16 flex flex-col items-center justify-between space-x-14 md:flex-row w-full max-w-[1275px]">
-					<div className="bg-gray w-full md:max-w-[525px] h-[400px] rounded-md" />
-					<div className="flex flex-col min-w-[50%]">
-						<div className="flex items-center justify-between">
-							<h1 className="text-[32px] font-bold my-8 text-white">I&apos;m a FULL-STACK DEVELOPER</h1>
-							<div className="flex justify-between items-center h-5">
-								<div
-									className="relative border-white border rounded-full w-[17px] h-[17px] cursor-pointer"
-									onClick={() => setSelectedState(1)}
-								>
-									<div className={`absolute rounded-full w-2.5 h-2.5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${selectedState === 1 ? "bg-white duration-[500ms] ease-in-out opacity-100" : "duration-[500ms] opacity-0 ease-in-out"}`}></div>
-								</div>
-								<hr className="border-white flex-1 h-[1px] min-w-[24px]" />
-								<div
-									className="relative border-white border rounded-full w-[17px] h-[17px] cursor-pointer"
-									onClick={() => setSelectedState(2)}
-								>
-									<div className={`absolute rounded-full w-2.5 h-2.5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${selectedState === 2 ? "bg-white duration-[500ms] ease-in-out opacity-100" : "duration-[500ms] opacity-0 ease-in-out"}`}></div>
-								</div>
-								<hr className="border-white flex-1 h-[1px] min-w-[24px]" />
-								<div
-									className="relative border-white border rounded-full w-[17px] h-[17px] cursor-pointer"
-									onClick={() => setSelectedState(3)}
-								>
-									<div className={`absolute rounded-full w-2.5 h-2.5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${selectedState === 3 ? "bg-white duration-[500ms] ease-in-out opacity-100" : "duration-[500ms] opacity-0 ease-in-out"}`}></div>
+				<div className="mt-12 flex flex-col items-center mx-[40px] sm:mx-[50px] xl:mx-[85px]">
+					<div className="flex flex-col items-center justify-between space-x-14 md:flex-row w-full max-w-[1275px]">
+						<div className="bg-gray w-full md:max-w-[525px] h-[400px] rounded-md" />
+						<div className="flex flex-col min-w-[50%] h-[400px] justify-between">
+							<div className="flex items-center justify-between">
+								<h1 className="text-[32px] font-bold my-8 text-white leading-[45px]">I&apos;m a FULL-STACK DEVELOPER</h1>
+								<div className="flex justify-between items-center h-5">
+									<div
+										className="relative border-white border rounded-full w-[17px] h-[17px] cursor-pointer"
+										onClick={() => setSelectedState(1)}
+									>
+										<div className={`absolute rounded-full w-2.5 h-2.5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${selectedState === 1 ? "bg-white duration-[500ms] ease-in-out opacity-100" : "duration-[500ms] opacity-0 ease-in-out"}`}></div>
+									</div>
+									<hr className="border-white flex-1 h-[1px] min-w-[24px]" />
+									<div
+										className="relative border-white border rounded-full w-[17px] h-[17px] cursor-pointer"
+										onClick={() => setSelectedState(2)}
+									>
+										<div className={`absolute rounded-full w-2.5 h-2.5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${selectedState === 2 ? "bg-white duration-[500ms] ease-in-out opacity-100" : "duration-[500ms] opacity-0 ease-in-out"}`}></div>
+									</div>
+									<hr className="border-white flex-1 h-[1px] min-w-[24px]" />
+									<div
+										className="relative border-white border rounded-full w-[17px] h-[17px] cursor-pointer"
+										onClick={() => setSelectedState(3)}
+									>
+										<div className={`absolute rounded-full w-2.5 h-2.5 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 ${selectedState === 3 ? "bg-white duration-[500ms] ease-in-out opacity-100" : "duration-[500ms] opacity-0 ease-in-out"}`}></div>
+									</div>
 								</div>
 							</div>
+							<article
+								ref={ref}
+								className="text-white min-h-[200px] max-w-[680px]"
+							>
+								{selectedState === 1 && (
+									<motion.p
+										initial={{ opacity: 0, y: 40 }}
+										animate={{ opacity: isInView && 1, y: isInView && 0 }}
+										transition={{ y: { duration: 0.4 }, delay: isInView && 0.1 }}
+										className="text-lg"
+									>
+										Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, quibusdam quos! Provident, sapiente cupiditate nisi quaerat consectetur quod. Magnam, accusamus.Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugit, quibusdam quos! Provident, sapiente cupiditate nisi quaerat consectetur quod. Magnam, accusamus. Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+										Fugit, quibusdam quos! Provident, sapiente cupiditate nisi quaerat consectetur quod. Magnam,
+									</motion.p>
+								)}
+								{selectedState === 2 && (
+									<motion.p
+										initial={{ opacity: 0, y: 40 }}
+										animate={{ opacity: isInView && 1, y: isInView && 0 }}
+										transition={{ y: { duration: 0.4 }, delay: isInView && 0.1 }}
+										className="text-lg"
+									>
+										Lorem ipsum dolor sit amet consectetur adipisicing e
+									</motion.p>
+								)}
+								{selectedState === 3 && (
+									<motion.p
+										initial={{ opacity: 0, y: 40 }}
+										animate={{ opacity: isInView && 1, y: isInView && 0 }}
+										transition={{ y: { duration: 0.4 }, delay: isInView && 0.1 }}
+										className="text-lg"
+									>
+										Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quas modi temporibus officiis nobis eveniet at dolores tenetur expedita fugit?
+									</motion.p>
+								)}
+							</article>
+							<a href="">
+								<button className="bg-gray px-[42px] py-[14px] w-fit rounded-md text-white font-bold select-none">Download CV</button>
+							</a>
 						</div>
-						<article
-							ref={ref}
-							className="text-white"
-						>
-							{selectedState === 1 && (
-								<motion.p
-									initial={{ opacity: 0, y: 40 }}
-									animate={{ opacity: isInView && 1, y: isInView && 0 }}
-									transition={{ y: { duration: 0.4 }, delay: isInView && 0.1 }}
-									className="text-lg"
-								>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quas modi temporibus officiis nobis eveniet at dolores tenetur expedita fugit?
-								</motion.p>
-							)}
-							{selectedState === 2 && (
-								<motion.p
-									initial={{ opacity: 0, y: 40 }}
-									animate={{ opacity: isInView && 1, y: isInView && 0 }}
-									transition={{ y: { duration: 0.4 }, delay: isInView && 0.1 }}
-									className="text-lg"
-								>
-									Lorem ipsum dolor sit amet consectetur adipisicing e
-								</motion.p>
-							)}
-							{selectedState === 3 && (
-								<motion.p
-									initial={{ opacity: 0, y: 40 }}
-									animate={{ opacity: isInView && 1, y: isInView && 0 }}
-									transition={{ y: { duration: 0.4 }, delay: isInView && 0.1 }}
-									className="text-lg"
-								>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloremque quas modi temporibus officiis nobis eveniet at dolores tenetur expedita fugit?
-								</motion.p>
-							)}
-						</article>
 					</div>
 				</div>
-			</div>
+			</section>
 		</>
 	);
 };
