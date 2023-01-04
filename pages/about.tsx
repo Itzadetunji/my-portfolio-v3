82893dd2571b60e64892c8e3ecec7b4a6f2b73d9
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-04 12:15:06 +0100

    Feat: Started work on portfolio card

--- pages/about.tsx
+++ pages/about.tsx
@@ -381,12 +381,15 @@ const About: NextPage = () => {
 			<section className="flex flex-col items-center px-5 sm:px-[50px] xl:px-[85px] mt-40">
 				<div className="flex flex-col space-y-12 w-full max-w-[1275px]">
 					<SectionTitle text={"My Portfolio"} />
-					<div className="h-[430px]">
+					<div className="relative h-[430px]">
 						<img
 							src="/images/portfolio.png"
 							alt=""
-							className="w-full h-full object-cover"
+							className="w-full h-full object-cover opacity-25  transition-opacity duration-500 ease-in-out hover:opacity-100 rounded-md shadow-lg shadow-blue/40"
 						/>
+						<div className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full py-8 px-4 flex flex-col items-center justify-center text-white">
+							
+						</div>
 					</div>
 				</div>
 			</section>
