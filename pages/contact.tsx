7cf01ac2d4c291244898472b80763e80bd676a31
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-12 00:17:17 +0100

    Feat: Fixed send email feature ✅

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -15,16 +15,50 @@ const Contact: NextPage = () => {
 		email: "",
 		body: "",
 	});
+	const [formState, setFormState] = useState<any>({
+		submitted: false,
+		success: false,
+		message: null,
+	});
+
+	const handleContactFormSubmit = async (e) => {
+		e.preventDefault();
+		try {
+			const response = await fetch("/api/contact", {
+				method: "POST",
+				headers: {
+					Accept: "application/json, text/plain, */*",
+					"Content-Type": "application/json",
+				},
+				body: JSON.stringify(formData),
+			});
+
+			if (response.status === 200) {
+				setFormState({ submitted: true, success: true });
+				setFormData({
+					name: "",
+					email: "",
+					subject: "",
+					body: "",
+				});
+				window.alert("Message sent!");
+			}
+		} catch (error) {
+			window.alert("Error Sending Message 😢. Try again 🤕.");
+			setFormState({
+				submitted: true,
+				success: false,
+				message: error.message,
+			});
+		}
+	};
 	return (
 		<>
 			<div className="mt-10">
 				<SectionTitle text="Let's chat" />
 				<form
 					action=""
-					onSubmit={(e) => {
-						e.preventDefault();
-						console.log(formData);
-					}}
+					onSubmit={handleContactFormSubmit}
 				>
 					<FormInput
 						placeholder={"Test"}
@@ -68,6 +102,7 @@ const Contact: NextPage = () => {
 					/>
 					<input
 						type="submit"
+						id="submit"
 						className="bg-red-900"
 					/>
 				</form>
@@ -86,7 +121,6 @@ const FormInput: NextPage<FormInputProps> = ({ placeholder, value, onChange }) =
 				placeholder={placeholder}
 				required
 				value={value}
-				// onKeyDown={() => console.log(value)}
 			/>
 		</>
 	);
