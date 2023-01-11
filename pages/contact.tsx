2e4266bcf0fc1a5a160f7c9186cd739242e3850b
Adetunji Adeyinka <adetunji.adeyinka@gardenventures.org>
2023-01-11 22:38:24 +0100

    Chore: updated contact page

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import { Navbar, Footer, SocialLinks } from "../components";
+import type { NextPage } from "next";
 
-const Contact = () => {
+const Contact: NextPage = () => {
 	return <></>;
 };
 export default Contact;
